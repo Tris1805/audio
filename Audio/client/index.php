24f5e9be92e8272485124ba9bb20f23b36f5b332
unknown <minhtri180522@gmail.com>
2023-04-06 07:26:23 +0700

    6/4/2023/2

--- Audio/client/index.php
+++ Audio/client/index.php
@@ -24,9 +24,9 @@
                 <div class="new-product">
                     <div class="new-product-title">SẢN PHẨM MỚI </div>
                     <div class="item-container">
-                        <div class="promo-item"><a href="chitietsanpham.html?productId=5"><img
+                        <div class="promo-item"><a href="chitietsanpham.php?id=5"><img
                                     src="../assets/images/products/sonywh1000xm4.jpg"></a></div>
-                        <div class="new-items"><a href="chitietsanpham.html?productId=1">
+                        <div class="new-items"><a href="chitietsanpham.php?id=1">
                                 <div class="new-items-img"><img src="../assets/images/products/hifiman_he400se.jpg" alt="">
                                 </div>
                                 <div class="new-items-data">
@@ -37,54 +37,54 @@
                                 </div>
                             </a>
                         </div>
-                        <div class="new-items"><a href="chitietsanpham.html?productId=9">
+                        <div class="new-items"><a href="chitietsanpham.php?id=9">
                                 <div class="new-items-img"><img src="../assets/images/products/airpod_3.jpg" alt=""></div>
                                 <div class="new-items-data">
-                                    <div class="new-items-data--title" href="chitietsanpham.html?productId=9">
+                                    <div class="new-items-data--title" href="chitietsanpham.php?id=9">
                                         <p>Apple Airpod 3</p>
                                     </div>
                                     <div class="newprice">4.750.000 đ</div>
                                 </div>
                             </a>
                         </div>
-                        <div class="new-items"><a href="chitietsanpham.html?productId=10">
+                        <div class="new-items"><a href="chitietsanpham.php?id=10">
                                 <div class="new-items-img"><img src="../assets/images/products/wf1000xm4.jpeg" alt="">
                                 </div>
                                 <div class="new-items-data">
-                                    <div class="new-items-data--title" href="chitietsanpham.html?productId=10">
+                                    <div class="new-items-data--title" href="chitietsanpham.php?id=10">
                                         <p>Sony WF-1000XM4</p>
                                     </div>
                                     <div class="newprice">5.490.000 đ</div>
                                 </div>
                             </a>
                         </div>
-                        <div class="new-items"><a href="chitietsanpham.html?productId=18">
+                        <div class="new-items"><a href="chitietsanpham.php?id=18">
                                 <div class="new-items-img"><img src="../assets/images/products/moondrop_kato.jpg" alt="">
                                 </div>
                                 <div class="new-items-data">
-                                    <div class="new-items-data--title" href="chitietsanpham.html?productId=18">
+                                    <div class="new-items-data--title" href="chitietsanpham.php?id=18">
                                         <p>Moondrop KATO</p>
                                     </div>
                                     <div class="newprice">4.350.000 đ</div>
                                 </div>
                             </a>
                         </div>
-                        <div class="new-items"><a href="chitietsanpham.html?productId=23">
+                        <div class="new-items"><a href="chitietsanpham.php?id=23">
                                 <div class="new-items-img"><img src="../assets/images/products/moondrop_blessing2.jpg"
                                         alt=""></div>
                                 <div class="new-items-data">
-                                    <div class="new-items-data--title" href="chitietsanpham.html?productId=23">
+                                    <div class="new-items-data--title" href="chitietsanpham.php?id=23">
                                         <p>Moondrop Blessing 2</p>
                                     </div>
                                     <div class="newprice">7.250.000 đ</div>
                                 </div>
                             </a>
                         </div>
-                        <div class="new-items"><a href="chitietsanpham.html?productId=3">
+                        <div class="new-items"><a href="chitietsanpham.php?id=3">
                                 <div class="new-items-img"><img src="../assets/images/products/focal_radiance.jpeg" alt="">
                                 </div>
                                 <div class="new-items-data">
-                                    <div class="new-items-data--title" href="chitietsanpham.html?productId=3">
+                                    <div class="new-items-data--title" href="chitietsanpham.php?id=3">
                                         <p>Focal Radiance</p>
                                     </div>
                                     <div class="newprice">29.990.000 đ</div>
@@ -100,44 +100,44 @@
                 <div class="new-product">
                     <div class="new-product-title">SẢN PHẨM NỔI BẬT </div>
                     <div class="item-container">
-                        <div class="new-items"><a href="chitietsanpham.html?productId=2">
+                        <div class="new-items"><a href="chitietsanpham.php?id=2">
                                 <div class="new-items-img"><img src="../assets/images/products/focal_celestee.jpg" alt="">
                                 </div>
                                 <div class="new-items-data">
-                                    <a class="new-items-data--title" href="chitietsanpham.html?productId=2">
+                                    <a class="new-items-data--title" href="chitietsanpham.php?id=2">
                                         <p>Focal Celestee</p>
                                     </a>
                                     <div class="newprice">29.990.000 đ</div>
                                 </div>
                             </a>
                         </div>
-                        <div class="new-items"><a href="chitietsanpham.html?productId=8">
+                        <div class="new-items"><a href="chitietsanpham.php?id=8">
                                 <div class="new-items-img"><img src="../assets/images/products/airpod_pro.jpg" alt="">
                                 </div>
                                 <div class="new-items-data">
-                                    <a class="new-items-data--title" href="chitietsanpham.html?productId=8">
+                                    <a class="new-items-data--title" href="chitietsanpham.php?id=8">
                                         <p>Apple Airpod Pro VN/A</p>
                                     </a>
                                     <div class="newprice">4.990.000 đ</div>
                                 </div>
                             </a>
                         </div>
-                        <div class="new-items"><a href="chitietsanpham.html?productId=11">
+                        <div class="new-items"><a href="chitietsanpham.php?id=11">
                                 <div class="new-items-img"><img src="../assets/images/products/wfc500.jpg" alt="">
                                 </div>
                                 <div class="new-items-data">
-                                    <a class="new-items-data--title" href="chitietsanpham.html?productId=11">
+                                    <a class="new-items-data--title" href="chitietsanpham.php?id=11">
                                         <p>Sony WF-C500</p>
                                     </a>
                                     <div class="newprice">1.990.000 đ</div>
                                 </div>
                             </a>
                         </div>
-                        <div class="new-items"><a href="chitietsanpham.html?productId=20">
+                        <div class="new-items"><a href="chitietsanpham.php?id=20">
                                 <div class="new-items-img"><img src="../assets/images/products/moondrop_starfield.png" alt="">
                                 </div>
                                 <div class="new-items-data">
-                                    <a class="new-items-data--title" href="chitietsanpham.html?productId=20">
+                                    <a class="new-items-data--title" href="chitietsanpham.php?id=20">
                                         <p>Moondrop Starfield</p>
                                     </a>
                                     <div class="newprice">2.890.000 đ</div>
